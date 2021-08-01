f7def4866e07bcb9f8ba02581e6bd2d00a05c87b
PlumHead69 <lenardf.horvath@gmail.com>
2021-08-01 11:20:18 +0200

    tron with AI

--- Tron/thron.py
+++ Tron/thron.py
@@ -1,115 +1,186 @@
-import curses as c
-from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
-import time
-screen = c.initscr()
-c.noecho()
-c.cbreak()
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
+import random
 
+WIDTH = 120
+HEIGHT = 30
+MAX_X = WIDTH - 2
+MAX_Y = HEIGHT - 2
+SNAKE_LENGTH = 1
 
-def snakeWin(snakeName):
-    
-    screen.keypad(True)
-    screen.addstr(0,0, snakeName)
-    screen.refresh()
-    time.sleep(10)
-    c.endwin()
+SNAKE_X = 1
+SNAKE_Y = 27
 
+SNAKEBOT_X = 2
+SNAKEBOT_Y = 10
 
-def main():
+TIMEOUT = 100
 
-    c.initscr()
-    win = c.newwin(30, 120, 0, 0)
-    win.keypad(1)
-    c.noecho()
-    c.curs_set(0)
-    win.border(0)
-    win.nodelay(1)
+global_body_list = []
 
-    key = KEY_RIGHT  # Initializing values
-    key2 = ord("d")
-    snake = [[4, 10], [4, 9], [4, 8]]  # Initial snake co-ordinates
-    snake2 = [[24, 10], [24, 9], [24, 8]] 
+class Snake(object):
+    REV_DIR_MAP = {
+        KEY_UP: KEY_DOWN, KEY_DOWN: KEY_UP,
+        KEY_LEFT: KEY_RIGHT, KEY_RIGHT: KEY_LEFT,
+    }
 
-    
+    def __init__(self, x, y, window):
+        self.hit_score = 0
+        self.timeout = TIMEOUT
+        self.body_list=[]
 
-    while key != 27:  # While Esc key is not pressed
-        win.border(0)
-        win.addstr(0, 27, ' TRON ')  # 'SNAKE' strings
-        win.timeout(
-            10
-        )  # Increases the speed of Snake as its length increases for speed
-
-        prevKey = key  # Previous key pressed
-        event = win.getch()
-        key = key if event == -1 else event
-
-        if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN,
-                       27]:  # If an invalid key is pressed
-            key = prevKey
-
-
-        prevKey2 = key2  # Previous key pressed
-        key2 = key2 if event == -1 else event
-
-
-        if key2 not in [ord("a"),ord("d"),ord("w"),ord("s"),
-                       27]:  # If an invalid key is pressed
-            key2 = prevKey2
-
-
-
-        # Calculates the new coordinates of the head of the snake.
-        # NOTE: len(snake) increases.
-
-        snake.insert(0, [
-            snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1),
-            snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)
-        ])
-
-        snake2.insert(0, [
-            snake2[0][0] + (key2 == ord("s") and 1) + (key2 ==ord("w") and -1),
-            snake2[0][1] + (key2 == ord("a") and -1) + (key2 == ord("d") and 1)
-        ])
-
-
-        # If snake crosses the boundaries, make it enter from the other side
-        if snake[0][0] == 0:
-            screen.keypad(True)
-            screen.addstr(0,0, snake2)
-            screen.refresh()
-            time.sleep(10)
-    
-        if snake[0][1] == 0:
-            snakeWin(snake2)
-        if snake[0][0] == 29:
-            snakeWin(snake2)
-        if snake[0][1] == 119:
-            snakeWin(snake2)
-        if snake[0] in snake[1:]:
-            snakeWin(snake2)
-        #snake2 dieing
-        if snake2[0][0] == 0:
-            snakeWin(snake)
-        if snake2[0][1] == 0:
-            snakeWin(snake)
-        if snake2[0][0] == 29:
-            snakeWin(snake)
-        if snake2[0][1] == 119:
-            snakeWin(snake)
-        if snake2[0] in snake2[1:]:
-            snakeWin(snake)
+        global_body_list.append(Body(x, y))
+        self.body_list.append(Body(x, y))
+        self.window = window    
+        self.direction = KEY_RIGHT
         
-        #snake collision
-        if snake2[0] in snake[1:]:
-            snakeWin(snake)
-        if snake[0] in snake2[1:]:
-            snakeWin(snake2)
+        self.direction_map = {
+            KEY_UP: self.move_up,
+            KEY_DOWN: self.move_down,
+            KEY_LEFT: self.move_left,
+            KEY_RIGHT: self.move_right
+        }
 
-        win.addch(snake[0][0], snake[0][1], 'X')
-        win.addch(snake2[0][0], snake2[0][1], 'O')
+    @property
+    def collided(self):
+        if self.head.y < 1:
+            return True
 
-    c.endwin()
+        if self.head.y > MAX_Y:
+            return True
+
+        if self.head.x < 1:
+            return True
+
+        if self.head.x > MAX_X:
+            return True
+
+        return any([body.coor == self.head.coor
+                    for body in global_body_list[:-1]])
+
+    @property
+    def obstacle(self):
+        if self.head.y == 1:
+            return True
+
+        if self.head.y == MAX_Y:
+            return True
+
+        if self.head.x == 1:
+            return True
+
+        if self.head.x == MAX_X:
+            return True
+
+        return any([body.coor == self.ahead()
+                    for body in global_body_list[:-1]])
+    
+    def ahead(self):
+        if self.direction==KEY_RIGHT:
+            self.ahead_coor = self.head.x+1,self.head.y
+            return self.ahead_coor
+
+        if self.direction==KEY_LEFT:
+            self.ahead_coor= self.head.x-1,self.head.y
+            return self.ahead_coor
+
+        if self.direction== KEY_DOWN:
+            self.ahead_coor = self.head.x,self.head.y+1
+            return self.ahead_coor
+
+        if self.direction== KEY_UP:
+            self.ahead_coor= self.head.x,self.head.y-1
+            return self.ahead_coor     
+
+
+    def update(self):
+        global_body_list.append(Body(self.head.x, self.head.y))
+        self.body_list.append(Body(self.head.x, self.head.y))
+        self.direction_map[self.direction]()
+       
+
+    def change_direction(self, direction):
+        if direction != Snake.REV_DIR_MAP[self.direction]:
+            self.direction = direction
+
+    def render(self):
+        for body in self.body_list:
+            self.window.addstr(body.y, body.x, body.char)
+
+    @property
+    def head(self):
+        return self.body_list[-1]
+
+    @property
+    def coor(self):
+        return self.head.x, self.head.y
+
+    def move_up(self):
+        self.head.y -= 1
+        
+
+    def move_down(self):
+        self.head.y += 1
+        
+
+    def move_left(self):
+        self.head.x -= 1
+        
+
+    def move_right(self):
+        self.head.x += 1
+        
+
+class Body(object):
+    def __init__(self, x, y, char='#'):
+        self.x = x
+        self.y = y
+        self.char = char
+
+    @property
+    def coor(self):
+        return self.x, self.y
 
 
 if __name__ == '__main__':
-    main()
+    curses.initscr()
+    curses.beep()
+    curses.beep()
+    window = curses.newwin(HEIGHT, WIDTH, 0, 0)
+    window.timeout(TIMEOUT)
+    window.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    window.border(0)
+    
+    snake = Snake(SNAKE_X, SNAKE_Y, window)
+    snake_bot=Snake(SNAKEBOT_X,SNAKEBOT_Y,window)
+    while True:
+        window.clear()
+        window.border(0)
+        snake.render()
+        snake_bot.render()
+        rDirection= random.choice([KEY_UP, KEY_LEFT,KEY_DOWN, KEY_RIGHT])
+        event = window.getch()
+
+        if event == 27:
+            break
+
+        if event in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
+            snake.change_direction(event)
+
+        if event == 32:
+            key = -1
+            while key != 32:
+                key = window.getch()
+
+        if snake_bot.obstacle == True:
+            snake_bot.change_direction(rDirection)
+
+        snake.update()
+        snake_bot.update()
+        if snake.collided:
+            break
+
+
+curses.endwin()
